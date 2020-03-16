aa49bf611d055001b02204bffbfd40ca87e794a2
Jay T <jaytula@gmail.com>
2020-03-16 03:58:06 -0700

    Base Features: Understanding JSX

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1>Hi, I'm a React App.</h1>
     </div>
   );
+  // return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Does this work now') );
 }
 
 export default App;
