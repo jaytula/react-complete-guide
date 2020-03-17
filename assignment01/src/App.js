b5c5bcf4dadc474968b6500b25fd037915011fba
Jay T <jaytula@gmail.com>
2020-03-17 00:01:05 -0700

    #1 Output multiple UserOutput's in App component

--- assignment01/src/App.js
+++ assignment01/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import UserOutput from './UserOutput';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <h1>Hello World!</h1>
+        <UserOutput />
+        <UserOutput />
+        <UserOutput />
       </div>
     );
   }
