3a77f087ea0e5f0537948d4f6c46d29b20e1b1ea
Jay T <jaytula@gmail.com>
2020-03-16 18:23:23 -0700

    Convert to class-based component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App.</h1>
-      <p>This is a really working!</p>
-      <Person name="Max" age="28" />
-      <Person name="Manu" age="29" >My Hobbies: Racing</Person>
-      <Person name="Stephanie" age="26" />
-
-    </div>
-  );
-  // return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Does this work now') );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hi, I'm a React App.</h1>
+        <p>This is a really working!</p>
+        <Person name='Max' age='28' />
+        <Person name='Manu' age='29'>
+          My Hobbies: Racing
+        </Person>
+        <Person name='Stephanie' age='26' />
+      </div>
+    );
+    // return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Does this work now') );
+  }
 }
 
 export default App;
