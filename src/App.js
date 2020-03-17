736f5672d242761076a4fc1e2229872bfe86dd6e
Jay T <jaytula@gmail.com>
2020-03-16 20:31:54 -0700

    Base Features: Passing Method References Between Components

--- src/App.js
+++ src/App.js
@@ -7,34 +7,44 @@ class App extends Component {
   state = {
     persons: [
       { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29},
-      { name: 'Stephanie', age: 25}
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 25 }
     ],
     otherState: 'some other value'
-  }
+  };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     console.log('Was clicked!');
     // DON'T DO THIS: this.state.persons[0].name = 'Maximillian';
     this.setState({
       persons: [
-        { name: 'Maximillian', age: 28 },
-        { name: 'Manu', age: 29},
-        { name: 'Stephanie', age: 27}
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
       ]
-    })
-  }
+    });
+  };
   render() {
     return (
       <div className='App'>
         <h1>Hi, I'm a React App.</h1>
         <p>This is a really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >
+        <button onClick={() => this.switchNameHandler('Baximillian!!')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Bax')}
+        >
           My Hobbies: Racing
         </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
     // return React.createElement('div', {className: "App"}, React.createElement('h1', null, 'Does this work now') );
