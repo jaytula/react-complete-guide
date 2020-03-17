8f04ba33881a346049b14df167ae10b4f49b8562
Jay T <jaytula@gmail.com>
2020-03-17 00:38:00 -0700

    #1 Add styling in two different ways

--- assignment01/src/UserOutput.js
+++ assignment01/src/UserOutput.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import './UserOutput.css';
 
 class UserOutput extends React.Component {
   render() {
     const { name } = this.props;
+    const style = {
+      backgroundColor: 'green',
+      color: 'white'
+    };
 
     return (
-      <div>
+      <div className='UserOutput'>
         <p>My name is {name}.</p>
-        <p>This is paragraph 2.</p>
+        <p style={style}>This is paragraph 2.</p>
       </div>
     );
   }
