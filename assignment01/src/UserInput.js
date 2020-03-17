e89503391a0249d54cf66f37851e9f7adbbf88f7
Jay T <jaytula@gmail.com>
2020-03-16 23:58:29 -0700

    #1 Created UserInput and UserOutput components

---
+++ assignment01/src/UserInput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class UserInput extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type='text' />
+      </div>
+    );
+  }
+}
+
+export default UserInput;
---
+++ assignment01/src/UserOutput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class UserOutput extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>This is paragraph 1.</p>
+        <p>This is paragraph 2.</p>
+      </div>
+    );
+  }
+}
+
+export default UserOutput;
