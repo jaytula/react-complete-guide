ad3a304e25cf910b673337e3929b5b81662b6a59
Jay T <jaytula@gmail.com>
2020-03-19 16:50:22 -0700

    Diving Deeper: Component Creation Lifecycle in Action

--- main-app/src/components/Persons/Persons.js
+++ main-app/src/components/Persons/Persons.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Person from './Person/Person';
 
-const Persons = (props) => (
-  <div>
-    {props.persons.map((person, index) => (
-      <Person
-        key={person.id}
-        name={person.name}
-        age={person.age}
-        click={props.clicked.bind(this, index)}
-        changed={event => props.changed(event, person.id)}
-      ></Person>
-    ))}
-  </div>
-);
+const Persons = props => {
+  console.log('[Persons.js] rendering...');
+  return (
+    <div>
+      {props.persons.map((person, index) => (
+        <Person
+          key={person.id}
+          name={person.name}
+          age={person.age}
+          click={props.clicked.bind(this, index)}
+          changed={event => props.changed(event, person.id)}
+        ></Person>
+      ))}
+    </div>
+  );
+};
 
 export default Persons;
