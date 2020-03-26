54bb806fc68c2abec2a69b31e56c3d36d6f88256
Jay T <jaytula@gmail.com>
2020-03-26 12:40:38 -0700

    Diving Deeper: Rendering Adjacent JSX Elements (Array)

--- main-app/src/components/Persons/Person/Person.js
+++ main-app/src/components/Persons/Person/Person.js
@@ -5,15 +5,13 @@ class Person extends React.Component {
   render() {
     console.log('[Person.js] rendering...');
     const props = this.props;
-    return (
-      <div className={classes.Person}>
-        <p onClick={() => props.click()}>
+    return [
+        <p key="i1" onClick={() => props.click()}>
           I'm a {props.name} and I am {props.age} years old!
-        </p>
-        <p>{props.children}</p>
-        <input type='text' onChange={props.changed} value={props.name} />
-      </div>
-    );
+        </p>,
+        <p key="i2" >{props.children}</p>,
+        <input key="i3" type='text' onChange={props.changed} value={props.name} />
+    ];
   }
 }
 
