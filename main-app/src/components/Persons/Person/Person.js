e8c0eb5c04484202b00dab6c2e77f428b1c8cfe4
Jay T <jaytula@gmail.com>
2020-03-26 13:12:23 -0700

    Diving Deeper: Rendering Adjacent JSX Elements (HOC Aux example)

--- main-app/src/components/Persons/Person/Person.js
+++ main-app/src/components/Persons/Person/Person.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import classes from './Person.module.css';
+import Aux from '../../../hoc/Aux';
 
 class Person extends React.Component {
   render() {
     console.log('[Person.js] rendering...');
     const props = this.props;
-    return [
-        <p key="i1" onClick={() => props.click()}>
+    return (
+      <Aux>
+        <p key='i1' onClick={() => props.click()}>
           I'm a {props.name} and I am {props.age} years old!
-        </p>,
-        <p key="i2" >{props.children}</p>,
-        <input key="i3" type='text' onChange={props.changed} value={props.name} />
-    ];
+        </p>
+        <p key='i2'>{props.children}</p>
+        <input
+          key='i3'
+          type='text'
+          onChange={props.changed}
+          value={props.name}
+        />
+      </Aux>
+    );
   }
 }
 
