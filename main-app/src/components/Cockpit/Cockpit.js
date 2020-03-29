5f9d2f48393116ccb1843afb14f3f3e3f44834b8
Jay T <jaytula@gmail.com>
2020-03-28 21:27:31 -0700

    Diving Deeper:  contextType & useContext()

--- main-app/src/components/Cockpit/Cockpit.js
+++ main-app/src/components/Cockpit/Cockpit.js
@@ -1,10 +1,13 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 import classes from './Cockpit.module.css';
 
 import AuthContext from '../../context/auth-context';
 
 const Cockpit = props => {
   const toggleBtnRef = useRef(null);
+  const authContext = useContext(AuthContext);
+
+  console.log({authContext});
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
 
@@ -39,9 +42,7 @@ const Cockpit = props => {
       <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
         Toggle Persons
       </button>
-      <AuthContext.Consumer>
-        {context => <button onClick={context.login}>Log In</button>}
-      </AuthContext.Consumer>
+        <button onClick={authContext.login}>Log In</button>
     </div>
   );
 };
--- main-app/src/components/Persons/Person/Person.js
+++ main-app/src/components/Persons/Person/Person.js
@@ -1,41 +1,45 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classes from './Person.module.css';
-import withClass from '../../../hoc/withClass';
-import Aux from '../../../hoc/Aux';
+import React from "react";
+import PropTypes from "prop-types";
+import classes from "./Person.module.css";
+import withClass from "../../../hoc/withClass";
+import Aux from "../../../hoc/Aux";
 
-import AuthContext from '../../../context/auth-context';
+import AuthContext from "../../../context/auth-context";
 
 class Person extends React.Component {
   constructor(props) {
     super(props);
     this.inputElementRef = React.createRef();
   }
+
+  static contextType = AuthContext;
+
   componentDidMount() {
     // document.querySelectorAll('input')[2].focus();
     // this.inputElement.focus();
     this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);
   }
   render() {
-    console.log('[Person.js] rendering...');
+    console.log("[Person.js] rendering...");
     const props = this.props;
     return (
       <Aux>
-        <AuthContext.Consumer>
-          {context =>
-            context.authenticated ? <p>Authenticated!</p> : <p>Please Login!</p>
-          }
-        </AuthContext.Consumer>
+        {this.context.authenticated ? (
+          <p>Authenticated!</p>
+        ) : (
+          <p>Please Login!</p>
+        )}
 
-        <p key='i1' onClick={() => props.click()}>
+        <p key="i1" onClick={() => props.click()}>
           I'm a {props.name} and I am {props.age} years old!
         </p>
-        <p key='i2'>{props.children}</p>
+        <p key="i2">{props.children}</p>
         <input
-          key='i3'
+          key="i3"
           // ref={inputEl => {this.inputElement = inputEl}}
           ref={this.inputElementRef}
-          type='text'
+          type="text"
           onChange={props.changed}
           value={props.name}
         />
