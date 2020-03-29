8b5434a11c7089cbb4404d88e59d4fde57fd1831
Jay T <jaytula@gmail.com>
2020-03-29 16:46:37 -0700

    Burger Builder: Starting Implementation of the The Burger Builder Container

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
-  return <div>
-    <Layout>
-      <p>Test</p>
-    </Layout>
-  </div>;
+  return (
+    <div>
+      <Layout>
+        <BurgerBuilder />
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
