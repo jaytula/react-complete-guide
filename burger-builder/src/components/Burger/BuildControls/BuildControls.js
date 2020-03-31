46313391854321646a0f1b123aa3d7a51025bcb0
Jay T <jaytula@gmail.com>
2020-03-30 23:35:17 -0700

    Burger Builder: Outputting Multiple Build Controls

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
 
-const BuildControls = (props) => {
-  return <div>
-  </div>
+import classes from './BuildControls.module.css';
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Meat', type: 'meat' },
+  { label: 'Cheese', type: 'cheese' }
+];
+
+const BuildControls = props => {
+  return (
+    <div className={classes.BuildControls}>
+      {controls.map(ctrl => (
+        <BuildControl
+          key={ctrl.type}
+          label={ctrl.label}
+          type={ctrl.type}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default BuildControls;
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from  '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
   state = {
@@ -17,12 +18,12 @@ class BurgerBuilder extends Component {
     updatedIngredents[name] += quantity;
     this.setState({ingredients: updatedIngredents});
   }
-  
+
   render() {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build controls</div>
+        <BuildControls />
       </Aux>
     );
   }
