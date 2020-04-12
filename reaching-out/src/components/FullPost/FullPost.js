685dbe7c386f635714b439d9fc51d5d2313e6970
Jay T <jaytula@gmail.com>
2020-04-11 22:52:54 -0700

    Reaching Out: Making a Post Selectable

--- reaching-out/src/components/FullPost/FullPost.js
+++ reaching-out/src/components/FullPost/FullPost.js
@@ -3,20 +3,20 @@ import React, { Component } from 'react';
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+  render() {
+    if (!this.props.id)
+      return <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
 
-        );
-        return post;
-    }
+    return (
+      <div className='FullPost'>
+        <h1>Title</h1>
+        <p>Body</p>
+        <div className='Edit'>
+          <button className='Delete'>Delete</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default FullPost;
