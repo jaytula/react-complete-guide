6552b7564ebeddd1ab64593a71cfc312d527dfaa
Jay T <jaytula@gmail.com>
2020-04-14 00:45:12 -0700

    Routing: Setting Up Links

--- reaching-out/src/containers/Blog/Blog.js
+++ reaching-out/src/containers/Blog/Blog.js
@@ -10,31 +10,31 @@ class Blog extends Component {
   state = {
     posts: [],
     selectedPostId: null,
-    error: false,
+    error: false
   };
 
   componentDidMount() {
     axios
       .get('/posts')
-      .then((response) => {
+      .then(response => {
         const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map((post) => {
+        const updatedPosts = posts.map(post => {
           return { ...post, author: 'Max' };
         });
         this.setState({ posts: updatedPosts });
       })
-      .catch((err) => {
+      .catch(err => {
         this.setState({ error: true });
       });
   }
 
-  postSelectedHandler = (id) => {
+  postSelectedHandler = id => {
     this.setState({ selectedPostId: id });
   };
   render() {
     let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
     if (!this.state.error) {
-      posts = this.state.posts.map((post) => {
+      posts = this.state.posts.map(post => {
         return (
           <Post
             key={post.id}
@@ -47,7 +47,19 @@ class Blog extends Component {
     }
 
     return (
-      <div>
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <a href='/'>Home</a>
+              </li>
+              <li>
+                <a href='/new-post'>New Post</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <section className='Posts'>{posts}</section>
         <section>
           <FullPost id={this.state.selectedPostId} />
