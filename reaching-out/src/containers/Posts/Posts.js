143c9b37df9fa4dec02c1de4c70dd897acfb7997
Jay T <jaytula@gmail.com>
2020-04-15 15:56:13 -0700

    Routing: Passing Route Parameters

--- reaching-out/src/containers/Posts/Posts.js
+++ reaching-out/src/containers/Posts/Posts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from '../../axios';
+import { Link } from 'react-router-dom';
 
 import Post from '../../components/Post/Post';
 
@@ -8,7 +9,7 @@ import './Posts.css';
 class Posts extends React.Component {
   state = {
     posts: [],
-    selectedPostId: null,
+    selectedPostId: null
   };
 
   componentDidMount() {
@@ -35,13 +36,15 @@ class Posts extends React.Component {
     let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
     if (!this.state.error) {
       posts = this.state.posts.map(post => {
+        console.log(post);
         return (
-          <Post
-            key={post.id}
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}
-          />
+          <Link key={post.id} to={`/${post.id}`}>
+            <Post
+              title={post.title}
+              author={post.author}
+              clicked={() => this.postSelectedHandler(post.id)}
+            />
+          </Link>
         );
       });
     }
