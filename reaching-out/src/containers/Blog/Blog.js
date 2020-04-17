2362732fc324cf34015d89d367e532289be24214
Jay T <jaytula@gmail.com>
2020-04-17 00:42:41 -0700

    Routing: Working with Guards

--- reaching-out/src/containers/Blog/Blog.js
+++ reaching-out/src/containers/Blog/Blog.js
@@ -6,6 +6,10 @@ import Posts from '../Posts/Posts';
 import NewPost from '../NewPost/NewPost';
 
 class Blog extends Component {
+  state = {
+    auth: false
+  };
+
   render() {
     return (
       <div className='Blog'>
@@ -42,7 +46,9 @@ class Blog extends Component {
         {/* <Route path='/' exact render={() => <h1>Home</h1>} />
         <Route path='/' render={() => <h1>Home2</h1>} /> */}
         <Switch>
-          <Route path='/new-post' component={NewPost} />
+          {this.state.auth ? (
+            <Route path='/new-post' component={NewPost} />
+          ) : null}
           <Route path='/posts' component={Posts} />
           {/* <Route path='/' component={Posts} /> */}
           <Redirect from='/' to='/posts' />
