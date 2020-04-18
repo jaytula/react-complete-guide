33d73e8d8e6fa410f5517919e3b8a5fe65f420fa
Jay T <jaytula@gmail.com>
2020-04-17 20:06:45 -0700

    Routing: Completed Assignment 3

--- routing--assignment-problem/src/containers/Course/Course.js
+++ routing--assignment-problem/src/containers/Course/Course.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 
 class Course extends Component {
     render () {
+      const courseId = this.props.match.params.id;
+
+      const searchParams = new URLSearchParams(this.props.location.search);
+      const title = searchParams.get('title');
         return (
             <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
+                <h1>{title}</h1>
+                <p>You selected the Course with ID: {courseId}</p>
             </div>
         );
     }
--- routing--assignment-problem/src/containers/Courses/Courses.js
+++ routing--assignment-problem/src/containers/Courses/Courses.js
@@ -1,30 +1,43 @@
 import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom';
 
 import './Courses.css';
+import Course from '../Course/Course';
 
 class Courses extends Component {
-    state = {
-        courses: [
-            { id: 1, title: 'Angular - The Complete Guide' },
-            { id: 2, title: 'Vue - The Complete Guide' },
-            { id: 3, title: 'PWA - The Complete Guide' }
-        ]
-    }
+  state = {
+    courses: [
+      { id: 1, title: 'Angular - The Complete Guide' },
+      { id: 2, title: 'Vue - The Complete Guide' },
+      { id: 3, title: 'PWA - The Complete Guide' },
+    ],
+  };
 
-    render () {
-        return (
-            <div>
-                <h1>Amazing Udemy Courses</h1>
-                <section className="Courses">
-                    {
-                        this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
-                        } )
-                    }
-                </section>
-            </div>
-        );
-    }
+  render() {
+    console.log(this.props);
+
+    return (
+      <div>
+        <h1>Amazing Udemy Courses</h1>
+        <section className='Courses'>
+          {this.state.courses.map(course => {
+            const params = new URLSearchParams();
+            params.append('title', course.title);
+            return (
+              <Link
+                key={course.id}
+                to={`/courses/${course.id}?${params.toString()}`}
+              >
+                <article className='Course'>{course.title}</article>
+              </Link>
+            );
+          })}
+        </section>
+
+        <Route path={this.props.match.url + '/:id'} component={Course} />
+      </div>
+    );
+  }
 }
 
 export default Courses;
