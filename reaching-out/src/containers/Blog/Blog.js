5b8a25b87b6f68e7e932efc2e297199348a60ae6
Jay T <jaytula@gmail.com>
2020-04-17 19:02:08 -0700

    Routing: Loading Routes Lazily

--- reaching-out/src/containers/Blog/Blog.js
+++ reaching-out/src/containers/Blog/Blog.js
@@ -3,7 +3,12 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from '../Posts/Posts';
-import NewPost from '../NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from '../NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+  return import('../NewPost/NewPost');
+})
 
 class Blog extends Component {
   state = {
@@ -47,7 +52,7 @@ class Blog extends Component {
         <Route path='/' render={() => <h1>Home2</h1>} /> */}
         <Switch>
           {this.state.auth ? (
-            <Route path='/new-post' component={NewPost} />
+            <Route path='/new-post' component={AsyncNewPost} />
           ) : null}
           <Route path='/posts' component={Posts} />
           <Route render={() => <h1>Not found</h1>} />
