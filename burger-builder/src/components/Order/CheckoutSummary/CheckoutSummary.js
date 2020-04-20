695726626a0ae33883237753b64a68658d0bce08
Jay T <jaytula@gmail.com>
2020-04-19 21:27:44 -0700

    Burger Routing: Navigating Back & To Next Page

--- burger-builder/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ burger-builder/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -12,11 +12,14 @@ const CheckoutSummary = props => {
       <div style={{ width: '100%', margin: 'auto' }}>
         <Burger ingredients={props.ingredients} />
       </div>
-      <Button btnType='Danger' clicked>CANCEL</Button>
-      <Button btnType='Success' clicked>CONTINUE</Button>
+      <Button btnType='Danger' clicked={props.onCheckoutCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType='Success' clicked={props.onCheckoutContinued}>
+        CONTINUE
+      </Button>
     </div>
   );
 };
-
 
 export default withRouter(CheckoutSummary);
--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -7,13 +7,26 @@ class Checkout extends React.Component {
       salad: 1,
       meat: 1,
       cheese: 1,
-      bacon: 1
-    }
+      bacon: 1,
+    },
+  };
+
+  checkoutCancelledHandler = () => {
+    this.props.history.goBack();
   }
+
+  checkoutContinuedHandler = () => {
+    this.props.history.replace('/checkout/contact-data');
+  }
+
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients} />
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
+          onCheckoutCancelled={this.checkoutCancelledHandler}
+          onCheckoutContinued={this.checkoutContinuedHandler}
+        />
       </div>
     );
   }
