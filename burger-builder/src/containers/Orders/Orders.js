e0e2c1d27268acd8266c82aefa0f4d051201b57c
Jay T <jaytula@gmail.com>
2020-04-23 11:56:49 -0700

    Burger Routing: Fetching Orders

--- burger-builder/src/containers/Orders/Orders.js
+++ burger-builder/src/containers/Orders/Orders.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true
+  };
+  componentDidMount() {
+    axios.get('/orders.json').then(res => {
+      this.setState({loading: false});
+      const { data } = res;
+      const orders = Object.keys(data).map(key => {
+        return { id: key, ...data[key] };
+      });
+      this.setState({ orders });
+      console.log(orders);
+    }).catch(err => {
+      this.setState({loading: false});
+    })
+  }
   render() {
     return (
       <div>
-        <Order />
-        <Order />
+        {this.state.orders.map(order => (
+          <Order
+            key={order.id}
+            ingredients={order.ingredients}
+            price={order.price}
+          />
+        ))}
       </div>
     );
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
