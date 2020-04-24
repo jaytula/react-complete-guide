38226f792def8327a54b1fd7b707da47cb3fad7e
Jay T <jaytula@gmail.com>
2020-04-23 21:04:11 -0700

    Burger Routing: Outputting the Orders

--- burger-builder/src/components/Order/Order.js
+++ burger-builder/src/components/Order/Order.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import classes from './Order.module.css';
+import React from "react";
+import classes from "./Order.module.css";
 
-const Order = props => {
+const Order = (props) => {
+  const ingredients = [];
+  for (let key in props.ingredients) {
+    ingredients.push({ name: key, amount: props.ingredients[key] });
+  }
+  const ingredientsOutput = ingredients.map((ig) => (
+    <span
+      key={ig.name}
+      style={{
+        border: "1px solid #ccc",
+        padding: "5px",
+        margin: "0 8px",
+        display: "inline-block",
+      }}
+    >
+      {ig.name} ({ig.amount})
+    </span>
+  ));
   return (
     <div className={classes.Order}>
-      <p>Ingredients: Salas (1)</p>
+      <p>Ingredients: {ingredientsOutput}</p>
       <p>
-        Price: <strong>USD 5.45</strong>
+        Price: <strong>USD {(+props.price).toFixed(2)}</strong>
       </p>
     </div>
   );
