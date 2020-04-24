d55baf47b5362b36601db39d3411dfd0e4e46d66
Jay T <jaytula@gmail.com>
2020-04-23 23:44:25 -0700

    Form Validation: Setting Up a JS Config for the Form

--- burger-builder/src/components/UI/Input/Input.js
+++ burger-builder/src/components/UI/Input/Input.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import classes from './Input.module.css';
 
-
 const Input = props => {
   let inputElement = null;
 
-  switch (props.inputtype) {
+  switch (props.elementType) {
     case 'textarea':
-      inputElement = <textarea className={classes.InputElement} {...props} />;
+      inputElement = (
+        <textarea className={classes.InputElement} {...props.elementConfig} />
+      );
       break;
     case 'input':
     default:
-      inputElement = <input className={classes.InputElement} {...props} />;
-    }
+      inputElement = (
+        <input
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+        />
+      );
+  }
   return (
     <div className={classes.Input}>
       <label className={classes.Label}>{props.label}</label>
--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -8,11 +8,57 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: '',
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name',
+        },
+        value: '',
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street',
+        },
+        value: '',
+      },
+      zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Postal Code',
+        },
+        value: '',
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country',
+        },
+        value: '',
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Your Mail',
+        },
+        value: '',
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            { value: 'fastest', displayValue: 'Fastest' },
+            { value: 'cheapest', displayValue: 'Cheapest' },
+          ],
+        },
+        value: '',
+      },
     },
     loading: false,
   };
@@ -24,16 +70,6 @@ class ContactData extends Component {
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.totalPrice,
-      customer: {
-        name: 'Max S',
-        address: {
-          street: 'Teststreet 1',
-          zipCode: '41351',
-          country: 'USA',
-        },
-        email: 'test@test.com',
-      },
-      deliveryMethod: 'fastest',
     };
 
     axios
@@ -50,10 +86,29 @@ class ContactData extends Component {
   render() {
     let form = (
       <form>
-        <Input inputtype="input" type='text' name='name' placeholder='Your Name' />
-        <Input inputtype="input" type='email' name='email' placeholder='Your Mail' />
-        <Input inputtype="input" type='text' name='street' placeholder='Street' />
-        <Input inputtype="input" type='text' name='postal' placeholder='Postal Code' />
+        <Input
+          elementType="..."
+          elementConfig="..."
+          value="..."
+        />
+        <Input
+          inputtype='input'
+          type='email'
+          name='email'
+          placeholder='Your Mail'
+        />
+        <Input
+          inputtype='input'
+          type='text'
+          name='street'
+          placeholder='Street'
+        />
+        <Input
+          inputtype='input'
+          type='text'
+          name='postal'
+          placeholder='Postal Code'
+        />
         <Button btnType='Success' clicked={this.orderHandler}>
           ORDER
         </Button>
