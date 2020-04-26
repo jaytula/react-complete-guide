0fdcd950daddba1a7831491f39ff1ed4efa66af0
Jay T <jaytula@gmail.com>
2020-04-26 10:45:50 -0700

    Redux: Adding Subscriptions

--- redux--01/redux-basics.js
+++ redux--01/redux-basics.js
@@ -1,12 +1,12 @@
 const redux = require('redux');
 
 const initialState = {
-  counter: 0
-}
+  counter: 0,
+};
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case 'INC_COUNTER':
       return { ...state, counter: state.counter + 1 };
     case 'ADD_COUNTER':
@@ -17,14 +17,14 @@ const rootReducer = (state = initialState, action) => {
 
 // Store
 const store = redux.createStore(rootReducer);
-
 console.log(store.getState());
+
+// Subscription
+store.subscribe(() => console.log('[Subscription]', store.getState()));
 
 // Dispatch Action
 
-store.dispatch({type: 'INC_COUNTER'});
-store.dispatch({type: 'ADD_COUNTER', value: 10});
-
+store.dispatch({ type: 'INC_COUNTER' });
 console.log(store.getState());
-
-// Subscribing
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
+console.log(store.getState());
