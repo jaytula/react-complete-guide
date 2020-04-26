5656df38e40b7c449932283353e4d5818c76c9cf
Jay T <jaytula@gmail.com>
2020-04-26 14:42:45 -0700

    Redux: Combining Local UI State and Redux

--- redux--assignment-2-problem/src/components/AddPerson/AddPerson.js
+++ redux--assignment-2-problem/src/components/AddPerson/AddPerson.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './AddPerson.css';
 
-const addPerson = props => (
-  <div className='AddPerson'>
-    <button onClick={props.personAdded}>Add Person</button>
-  </div>
-);
+class AddPerson extends Component {
+  state = {
+    name: 'Max',
+    age: 28,
+  };
 
-export default addPerson;
+  onNameChangeHandler = event => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
+  onAgeChangeHandler = event => {
+    this.setState({
+      age: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className='AddPerson'>
+        <input
+          type='text'
+          value={this.state.name}
+          onChange={this.onNameChangeHandler}
+          placeholder='Name'
+        />
+        <input
+          type='number'
+          value={this.state.age}
+          placeholder='age'
+          onChange={this.onAgeChangeHandler}
+        />
+        <button
+          onClick={() =>
+            this.props.personAdded(this.state.name, this.state.age)
+          }
+        >
+          Add Person
+        </button>
+      </div>
+    );
+  }
+}
+
+export default AddPerson;
--- redux--assignment-2-problem/src/containers/Persons.js
+++ redux--assignment-2-problem/src/containers/Persons.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux'
+import { connect } from 'react-redux';
 
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
@@ -7,11 +7,11 @@ import AddPerson from '../components/AddPerson/AddPerson';
 import * as actionTypes from '../store/actions';
 
 class Persons extends Component {
-  personAddedHandler = () => {
+  personAddedHandler = (name, age) => {
     const newPerson = {
       id: Math.random(), // not really unique but good enough here!
-      name: 'Max',
-      age: Math.floor(Math.random() * 40),
+      name,
+      age,
     };
 
     this.props.addPersonHandler(newPerson);
@@ -39,12 +39,13 @@ class Persons extends Component {
 }
 
 const mapStateToProps = state => ({
-  persons: state
+  persons: state,
 });
 
 const mapDispatchToProps = dispatch => ({
-  addPersonHandler: (payload) => dispatch({type: actionTypes.ADD_PERSON, payload}),
-  deletePersonHandler: (id) => dispatch({type: actionTypes.REMOvE_PERSON, id})
+  addPersonHandler: payload =>
+    dispatch({ type: actionTypes.ADD_PERSON, payload }),
+  deletePersonHandler: id => dispatch({ type: actionTypes.REMOvE_PERSON, id }),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Persons);
