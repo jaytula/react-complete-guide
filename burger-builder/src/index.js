10c1046dc57aaece0f99df4558307e30b6a08de0
Jay T <jaytula@gmail.com>
2020-04-27 11:35:13 -0700

    Add support for Redux Dev Tools

--- burger-builder/src/index.js
+++ burger-builder/src/index.js
@@ -9,15 +9,18 @@ import { BrowserRouter } from 'react-router-dom';
 
 import rootReducer from './store/reducer';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   // <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   // </React.StrictMode>,
   document.getElementById('root')
 );
