53e4884e530d2170d17e065a4361c57b28563eff
Jay T <jaytula@gmail.com>
2020-04-27 11:52:21 -0700

    Adding Redux: Finishing the Reducer for Ingredients

--- burger-builder/src/store/reducer.js
+++ burger-builder/src/store/reducer.js
@@ -1,13 +1,29 @@
 import * as actionTypes from './actions';
 
-const initialState = { ingredients: null, totalPrice: 4 };
+const initialState = {
+  ingredients: { cheese: 0, meat: 0, salad: 0, bacon: 0 },
+  totalPrice: 4,
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_INGREDIENT:
-      break;
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+        },
+      };
+
     case actionTypes.REMOVE_INGREDIENT:
-      break;
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
+        },
+      };
     default:
       return state;
   }
