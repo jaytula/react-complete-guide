0667bc1fb62936cde513a6aa2416c943173e2dd1
Jay T <jaytula@gmail.com>
2020-05-04 23:19:27 -0700

    Authentication: Getting a Token from the Backend

--- burger-builder/src/store/actions/auth.js
+++ burger-builder/src/store/actions/auth.js
@@ -1,28 +1,45 @@
 import * as actionTypes from './actionTypes';
+import Axios from 'axios';
+
+const API_KEY = process.env.REACT_APP_FIREBASE_API_KEY;
+const AUTH_ENDPOINT =
+  'https://identitytoolkit.googleapis.com/v1/accounts:signUp';
 
 const authStart = () => {
   return {
-    type: actionTypes.AUTH_START
-  }
-}
+    type: actionTypes.AUTH_START,
+  };
+};
 
-const authSuccess = (authData) => {
+const authSuccess = authData => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData
-  }
-}
+    authData,
+  };
+};
 
-const authFail = (error) => {
+const authFail = error => {
   return {
     type: actionTypes.AUTH_FAIL,
-    error
-  }
-}
+    error,
+  };
+};
 
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
+    const authData = {
+      email,
+      password,
+      returnSecureToken: true
+    }
 
-  }
-}
+    Axios.post(AUTH_ENDPOINT, authData, { params: { key: API_KEY } }).then( response => {
+      console.log(response);
+      dispatch(authSuccess(response.data));
+    }).catch(err => {
+      console.log(err);
+      dispatch(authFail(err))
+    })
+  };
+};
