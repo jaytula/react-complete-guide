33b41542cd7ef4e56fce3f7f1c9bc7b148f36419
Jay T <jaytula@gmail.com>
2020-05-16 14:42:32 -0700

    Pretty NavigationItems.test.js

--- burger-builder/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ burger-builder/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 import { configure, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import NavigationItems from "./NavigationItems";
-import NavigationItem from './NavigationItem/NavigationItem';
+import NavigationItem from "./NavigationItem/NavigationItem";
 
 configure({ adapter: new Adapter() });
 
@@ -10,15 +10,15 @@ describe("<NavigationItems />", () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<NavigationItems />)
-  })
-  
+    wrapper = shallow(<NavigationItems />);
+  });
+
   it("should render two <NavigationItem /> elements if not authenticated", () => {
-      expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    expect(wrapper.find(NavigationItem)).toHaveLength(2);
   });
 
   it("should render three <NavigationItem /> elements if authenticated", () => {
-    wrapper.setProps({isAuth: true})
+    wrapper.setProps({ isAuth: true });
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
-});
+  });
 });
