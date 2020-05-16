51c5c43682ede36efac380c6a02b54f1eafd0981
Jay T <jaytula@gmail.com>
2020-05-15 22:55:17 -0700

    Improving Burger: Adding Lazy Loading

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import { Route, Switch, Redirect } from 'react-router-dom';
-import Layout from './hoc/Layout/Layout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
-import Logout from './containers/Auth/Logout/Logout';
-import * as actions from './store/actions/index';
-import { connect } from 'react-redux';
+import React from "react";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Layout from "./hoc/Layout/Layout";
+import Logout from "./containers/Auth/Logout/Logout";
+import * as actions from "./store/actions/index";
+import { connect } from "react-redux";
+import asyncComponent from "./hoc/asyncComponent/asyncComponent";
+
+const asyncCheckout = asyncComponent(() => {
+  return import("./containers/Checkout/Checkout");
+});
+
+const asyncOrders = asyncComponent(() => {
+  return import("./containers/Orders/Orders");
+});
+
+const asyncAuth = asyncComponent(() => {
+  return import("./containers/Auth/Auth");
+});
 
 class App extends React.Component {
   // state = {
@@ -26,20 +36,20 @@ class App extends React.Component {
   render() {
     let routes = (
       <Switch>
-        <Route path='/auth' component={Auth} />
-        <Route path='/' exact component={BurgerBuilder} />
-        <Redirect to='/' />
+        <Route path="/auth" component={asyncAuth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
       </Switch>
     );
     if (this.props.isAuthenticated) {
       routes = (
         <Switch>
-          <Route path='/checkout' component={Checkout} />
-          <Route path='/orders' component={Orders} />
-          <Route path='/logout' component={Logout} />
-          <Route path='/auth' component={Auth} />
-          <Route path='/' exact component={BurgerBuilder} />
-          <Redirect to='/' />
+          <Route path="/checkout" component={asyncCheckout} />
+          <Route path="/orders" component={asyncOrders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/auth" component={asyncAuth} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/" />
         </Switch>
       );
     }
@@ -51,11 +61,11 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   isAuthenticated: !!state.auth.token,
 });
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
     onTryAutoSignup: () => dispatch(actions.authCheckState()),
   };
--- burger-builder/src/index.js
+++ burger-builder/src/index.js
@@ -14,9 +14,9 @@ import authReducer from './store/reducers/auth';
 import thunk from 'redux-thunk';
 
 const composeEnhancers =
-  process.env.NODE_ENV === 'development'
+  (process.env.NODE_ENV === 'development'
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose;
+    : null) || compose;
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
