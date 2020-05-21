2edefa7bde6077a37bef7cd8dad0ce1cd361f8c5
Jay T <jaytula@gmail.com>
2020-05-21 13:20:58 -0700

    Next.js: Components & Pages

--- working-with-nextjs/pages/auth/index.js
+++ working-with-nextjs/pages/auth/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import User from '../../components/User';
 
-const indexPage = () => (
+const authIndexPage = () => (
   <div>
     <h1>Auth Page</h1>
+    <User name="max" age="31" />
   </div>
 )
 
-export default indexPage
+export default authIndexPage
