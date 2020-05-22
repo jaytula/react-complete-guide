c3dddf8c45fcd7e1d48433f6d7869e9e09860c31
Jay T <jaytula@gmail.com>
2020-05-21 18:06:31 -0700

    Next.js: Handling (404) Errors

---
+++ working-with-nextjs/pages/_error.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "next/link";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>Oops, something went wrong</h1>
+      <p>
+        Try{" "}
+        <Link href="/auth">
+          <a>going back</a>
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default ErrorPage;
