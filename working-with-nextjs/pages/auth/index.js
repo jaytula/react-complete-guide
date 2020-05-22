e6fed895cc712c18979345383158073954f4162a
Jay T <jaytula@gmail.com>
2020-05-21 19:00:40 -0700

    Next.js: A Special Lifecycle Hook

--- working-with-nextjs/pages/auth/index.js
+++ working-with-nextjs/pages/auth/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import User from '../../components/User';
+import React from "react";
+import User from "../../components/User";
 
-const authIndexPage = () => (
+const authIndexPage = (props) => (
   <div>
-    <h1>Auth Page</h1>
+    <h1>Auth Page: {props.appName}</h1>
     <User name="max" age="31" />
   </div>
-)
+);
 
-export default authIndexPage
+authIndexPage.getInitialProps = async (context) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ appName: "Super App (Auth)" });
+    }, 1000);
+  });
+  return promise;
+};
+
+export default authIndexPage;
