a6ee7dcf491df56781e9b5ada238c29246c31836
Jay T <jaytula@gmail.com>
2020-05-23 09:22:13 -0700

    Animations: Wrapping the Transition Component

--- animations/src/App.js
+++ animations/src/App.js
@@ -52,16 +52,7 @@ class App extends Component {
           )}
         </Transition>
 
-        <Transition
-          mountOnEnter
-          unmountOnExit
-          in={this.state.modalIsOpen}
-          timeout={300}
-        >
-          {(state) => {
-            return <Modal show={state} closed={this.closeModal} />;
-          }}
-        </Transition>
+        <Modal in={this.state.modalIsOpen} closed={this.closeModal} />;
         {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>
           Open Modal
