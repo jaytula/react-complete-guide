9a7f0ab59dabb7e4b40a3fe8fbaa949c64084ac4
Jay T <jaytula@gmail.com>
2020-05-30 10:24:50 -0700

    React Hooks: Sending Http Requests

--- hooks-01/src/components/Ingredients/Ingredients.js
+++ hooks-01/src/components/Ingredients/Ingredients.js
@@ -8,15 +8,23 @@ const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
 
   const addIngredientHandler = ingredient => {
-    setIngredients(prevState => [
-      ...prevState,
-      { id: Math.random().toString(), ...ingredient },
-    ]);
+    fetch(`${process.env.REACT_APP_BACKEND}/ingredients.json`, {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      header: { 'Content-Type': 'application/json' },
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+      setIngredients(prevState => [
+        ...prevState,
+        { id: responseData.name, ...ingredient },
+      ]);
+    });
   };
 
-  const onRemoveItemHandler = (id) => {
-    setIngredients(prevState => prevState.filter(ing => ing.id !== id))
-  }
+  const onRemoveItemHandler = id => {
+    setIngredients(prevState => prevState.filter(ing => ing.id !== id));
+  };
   return (
     <div className='App'>
       <IngredientForm onAddIngredient={addIngredientHandler} />
@@ -24,7 +32,10 @@ const Ingredients = () => {
       <section>
         <Search />
         {/* Need to add list here! */}
-        <IngredientList ingredients={ingredients} onRemoveItem={onRemoveItemHandler} />
+        <IngredientList
+          ingredients={ingredients}
+          onRemoveItem={onRemoveItemHandler}
+        />
       </section>
     </div>
   );
