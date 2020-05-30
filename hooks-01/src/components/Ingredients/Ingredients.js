fdfb18c2b73e9b25b8b79c6042749974d5906809
Jay T <jaytula@gmail.com>
2020-05-29 21:22:29 -0700

    React Hooks: Passing State Data Across Components

--- hooks-01/src/components/Ingredients/Ingredients.js
+++ hooks-01/src/components/Ingredients/Ingredients.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
+import IngredientList from './IngredientList';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [ingredients, setIngredients] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    setIngredients(prevState => [
+      ...prevState,
+      { id: Math.random().toString(), ...ingredient },
+    ]);
+  };
   return (
-    <div className="App">
-      <IngredientForm />
+    <div className='App'>
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
         {/* Need to add list here! */}
+        <IngredientList ingredients={ingredients} onRemoveItem={() => {}} />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
