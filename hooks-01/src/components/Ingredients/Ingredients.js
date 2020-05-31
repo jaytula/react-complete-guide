7efa080f05edfd3ee87045ddbce45fdc616d30c9
Jay T <jaytula@gmail.com>
2020-05-30 21:35:45 -0700

    React Hooks: useEffect() & Loading Data

--- hooks-01/src/components/Ingredients/Ingredients.js
+++ hooks-01/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
@@ -7,19 +7,38 @@ import IngredientList from './IngredientList';
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
 
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_BACKEND}/ingredients.json`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(data => {
+        const ings = Object.entries(data).map(([id, value]) => ({
+          id,
+          ...value,
+        }));
+        setIngredients(ings);
+      });
+  }, []);
+
   const addIngredientHandler = ingredient => {
     fetch(`${process.env.REACT_APP_BACKEND}/ingredients.json`, {
       method: 'POST',
       body: JSON.stringify(ingredient),
       header: { 'Content-Type': 'application/json' },
-    }).then(response => {
-      return response.json();
-    }).then(responseData => {
-      setIngredients(prevState => [
-        ...prevState,
-        { id: responseData.name, ...ingredient },
-      ]);
-    });
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(responseData => {
+        setIngredients(prevState => [
+          ...prevState,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
   };
 
   const onRemoveItemHandler = id => {
