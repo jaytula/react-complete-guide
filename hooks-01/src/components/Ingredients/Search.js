1b9204eeface7589526764367ba4673020bcda46
Jay T <jaytula@gmail.com>
2020-06-01 13:39:07 -0700

    React Hooks: Cleaning Up with useEffect()

--- hooks-01/src/components/Ingredients/Search.js
+++ hooks-01/src/components/Ingredients/Search.js
@@ -8,28 +8,30 @@ const Search = React.memo(({ setIngredients }) => {
   const inputRef = useRef();
 
   useEffect(() => {
-    setTimeout(() => {
-      if(inputRef.current.value !== enteredFilter) return;
+    const timer = setTimeout(() => {
+      if (inputRef.current.value !== enteredFilter) return;
       const query =
-      enteredFilter.length === 0
-        ? ''
-        : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch(`${process.env.REACT_APP_BACKEND}/ingredients.json` + query, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(res => res.json())
-      .then(data => {
-        const ings = Object.entries(data).map(([id, value]) => ({
-          id,
-          ...value,
-        }));
-        setIngredients(ings);
-      });
+        enteredFilter.length === 0
+          ? ''
+          : `?orderBy="title"&equalTo="${enteredFilter}"`;
+      fetch(`${process.env.REACT_APP_BACKEND}/ingredients.json` + query, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(res => res.json())
+        .then(data => {
+          const ings = Object.entries(data).map(([id, value]) => ({
+            id,
+            ...value,
+          }));
+          setIngredients(ings);
+        });
+      return () => {
+        clearTimeout(timer);
+      };
     }, 500);
-
   }, [enteredFilter, setIngredients, inputRef]);
 
   return (
