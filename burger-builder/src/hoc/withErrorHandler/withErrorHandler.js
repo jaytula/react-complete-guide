b6b4fdcfb54f05908ed52b3edf0eafdd1a15b8aa
Jay T <jaytula@gmail.com>
2020-06-07 10:01:44 -0700

    Remove unused import

--- burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Aux/Aux';
 
