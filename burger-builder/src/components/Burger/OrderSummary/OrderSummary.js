96466a0285be48f8e5d44e8a9598d031988111b4
Jay T <jaytula@gmail.com>
2020-06-08 13:18:23 -0700

    Burger Builder with Hooks: Using React.memo() & More!

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,40 +2,36 @@ import React from 'react';
 import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-class OrderSummary extends React.Component {
+const OrderSummary = props => {
   // This could be a functional componen, doesnt' have to be a class
 
-  render() {
-    const ingredientsSummary = Object.keys(this.props.ingredients).map(
-      (igKey) => {
-        return (
-          <li key={igKey}>
-            <span style={{ textTransform: 'capitalize' }}>
-              {igKey}: {this.props.ingredients[igKey]}
-            </span>
-          </li>
-        );
-      }
-    );
-
+  const ingredientsSummary = Object.keys(props.ingredients).map(igKey => {
     return (
-      <Aux>
-        <h3>Your Order</h3>
-        <p>A delicious burger with the following ingredients:</p>
-        <ul>{ingredientsSummary}</ul>
-        <p>
-          <strong>Total Price: {this.props.price.toFixed(2)}</strong>
-        </p>
-        <p>Continue to Checkout?</p>
-        <Button btnType='Danger' clicked={this.props.purchaseCanceled}>
-          CANCEL
-        </Button>
-        <Button btnType='Success' clicked={this.props.purchaseContinued}>
-          CONTINUE
-        </Button>
-      </Aux>
+      <li key={igKey}>
+        <span style={{ textTransform: 'capitalize' }}>
+          {igKey}: {props.ingredients[igKey]}
+        </span>
+      </li>
     );
-  }
-}
+  });
+
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>{ingredientsSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}</strong>
+      </p>
+      <p>Continue to Checkout?</p>
+      <Button btnType='Danger' clicked={props.purchaseCanceled}>
+        CANCEL
+      </Button>
+      <Button btnType='Success' clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
+    </Aux>
+  );
+};
 
 export default OrderSummary;
